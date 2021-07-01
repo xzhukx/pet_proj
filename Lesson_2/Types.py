627ba63cf3632c038d7da51450aa33f8152e9e55
Denys Piliaiev <denyspiliaiev@Denyss-MacBook-Air.local>
2021-07-01 22:09:06 +0300

    Last V

--- Lesson_2/Types.py
+++ Lesson_2/Types.py
@@ -1,1 +1,29 @@
-name = input(float(("Enter the number: "))
+num_1 = input("Enter the number1: ")
+operator = input("Enter the operator: ")
+num_2 = input("Enter the number2: ")
+
+try:
+    num_1 = float(num_1)
+except Exception as e:
+    print(e)
+    exit()
+
+if num_1.isdigit() and num_2.isdigit():
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+    if operator == "-":
+        a=num_1-num_2
+        print(a)
+    elif operator == "+":
+        a=num_1+num_2
+        print(a)
+    elif operator == "*":
+        a=num_1*num_2
+        print(a)
+    elif operator == "/":
+        a=num_1/num_2
+        print(a)
+    else:
+        print("Incorrect operator")
+else:
+    print("Please print integer")
