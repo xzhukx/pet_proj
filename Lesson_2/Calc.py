7642aae7100f0d0890037cd473ae7da72896369e
denyspiliaiev <denis.pilyaev@gmail.com>
2021-07-19 09:03:43 +0300

    with def_func

--- Lesson_2/Calc.py
+++ Lesson_2/Calc.py
@@ -1,24 +1,41 @@
 num_1 = input("Enter the number1: ")
-operator = input("Enter the operator: ")
+oper = input("Enter the operator: ")
 num_2 = input("Enter the number2: ")
 
-try:
-    num_1 = float(num_1)
-    num_2 = float(num_2)
+def num(a):
+    try:
+        a = float(a)
+        return a
+    except Exception as x:
+        print("Hey man, do you think it`s funny ? this is not a number")
+        a = input("Enter the number1: ")
+        exit()
+
+def calculation(operator,n1,n2):
+    n1=num(n1)
+    n2=num(n2)
     if operator == "-":
-        a=num_1-num_2
+        a=n1-n2
         print(a)
     elif operator == "+":
-        a=num_1+num_2
+        a=n1+n2
         print(a)
     elif operator == "*":
-        a=num_1*num_2
+        a=n1*n2
         print(a)
     elif operator == "/":
-        a=num_1/num_2
-        print(a)
+        if n2 == 0:
+            print("Are you kidding me ? You can not divide by zero")
+            n2 = input("Enter the number2: you remember no zero here: ")
+            n2 = float(n2)
+            a = n1 / n2
+            print(a)
+        else:
+            a=n1/n2
+            print(a)
     else:
         print("Please correct the inputed operator")
-except Exception as e:
-    print("Please correct the inputed numbers")
-    exit()
+
+def next_step(result,n_num):
+
+calculation(oper, num_1, num_2)
