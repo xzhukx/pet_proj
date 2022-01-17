1cf9366996c5edd557a51be0618797e2774e9f8c
denyspiliaiev <denis.pilyaev@gmail.com>
2022-01-17 21:23:02 +0200

    with def_func

--- Parser/Gar_pok+pars.py
+++ Parser/Gar_pok+pars.py
@@ -1,12 +1,29 @@
 from bs4 import BeautifulSoup
-import requests
-from selenium import webdriver
-import time
+import requests as re
 
-soup = BeautifulSoup("https://www.gpee.com.ua/publication?id=1", "lxml")
-print(soup)
+web = re.get("https://www.oree.com.ua/")
+
+soup = BeautifulSoup(web.text, features="html5lib")
+
+# # needed_info = soup.find(id="index_col_group")
+
+# # price_type = needed_info.find_all("div", "small-caption pr-1")
+# # prices = needed_info.find_all("span", "index-prices-value")
+# # countries = needed_info.find_all("div", "index-country-name")
 
 
+needed_info = soup.find(id="index_col_group")
 
-last_heigh=browser.execute_script("https://www.gpee.com.ua/assets/js/news/get_more_news.js?v=82")
-print(last_heigh)
+price_type = needed_info.find_all("div", "small-caption pr-1")
+prices = needed_info.find_all("span", "index-prices-value")
+countries = needed_info.find_all("div", "index-country-name")
+
+zipped = (price_type, prices, countries)
+
+iter_zip = iter(zipped)
+
+for x in iter_zip:
+    print(x)
+
+# найти класс "роу", написать через "next" итератор
+
